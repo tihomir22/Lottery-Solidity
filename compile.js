fc6d06e663b2c3bcfc4f4968850491d70bda190a
Tihomir Stoychev <tihomir_alcudia3@hotmail.com>
2021-05-08 19:01:59 +0200

    Added tests for lottery

--- compile.js
+++ compile.js
@@ -2,13 +2,13 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const inboxPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(inboxPath, "utf-8");
 
 let input = {
   language: "Solidity",
   sources: {
-    "Inbox.sol": {
+    "Loterry.sol": {
       content: source,
     },
   },
@@ -21,7 +21,7 @@ let input = {
   },
 };
 
-module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts["Inbox.sol"];
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts["Loterry.sol"];
 
 /*for (var contractName in output.contracts["Inbox.sol"]) {
   console.log(contractName + ": " + output.contracts["Inbox.sol"][contractName].evm.bytecode.object);
--- deploy.js
+++ deploy.js
@@ -5,15 +5,15 @@ const obj = require("./compile");
 const provider = new HDWalletProvider(process.env.NEUMONIC, "https://rinkeby.infura.io/v3/56db03b29f7e42c4a2b892a70ad5ad97");
 const web3 = new Web3(provider);
 
-let interface = obj["InboxContract"].abi;
-let bytecode = obj["InboxContract"].evm.bytecode.object;
+let interface = obj["LoterryContract"].abi;
+let bytecode = obj["LoterryContract"].evm.bytecode.object;
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log("Attempting to deploy from account", accounts[0]);
 
   let contract = await new web3.eth.Contract(interface)
-    .deploy({ data: bytecode, arguments: ["Mi revolver te mata"] })
+    .deploy({ data: bytecode })
     .send({ gas: "1000000", from: accounts[0] });
 
   console.log("Deployed hash", contract.options.address);
